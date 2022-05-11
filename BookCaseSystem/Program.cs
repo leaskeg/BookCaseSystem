943fb563bb3680e85b4e47357e121f719555161a
Jens Bach <leaskegaming@gmail.com>
2022-05-11 15:47:23 +0200

    Add project files.

---
+++ BookCaseSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookCaseSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
